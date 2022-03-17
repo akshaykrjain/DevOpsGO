7a3bd12445abb35c569c5afe62184c8b49c39a6e
akshaykrjain <akshay.kumar.jain@mimiro.no>
2022-03-17 11:47:04 +0100

    Initial Release

---
+++ ec2manager/go.mod
@@ -0,0 +1,3 @@
+module github.com/akshaykrjain/DevOpsGO/ec2manager
+
+go 1.18
---
+++ ec2manager/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	//"github.com/aws/aws-sdk-go-v2/aws"
+	//"github.com/aws/aws-sdk-go-v2/config"
+	//"github.com/aws/aws-sdk-go-v2/service/ec2"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+	log.Println("work in progress..")
+}
